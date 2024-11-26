c6f076dfb2dd9abfbb30a0eb65dfaf3e908a9636
abidsarkar <abidsarkar0@gmail.com>
2024-11-26 22:47:27 +0600

    copy the menu for mobile view from the existed html desktop using js. so that we don't need to write again

---
+++ script.js
@@ -0,0 +1,17 @@
+// Copy menu for mobile
+function copyMenu(){
+    //copy inside ..dpt-cat to .departments
+    let dptCategory = document.querySelector(".dpt-cat");
+    let dptPlace = document.querySelector(".departments");
+    dptPlace.innerHTML = dptCategory.innerHTML;
+    // copy inside nav to nav
+    let mainNav = document.querySelector(".header-nav nav");
+    let navPlace = document.querySelector(".off-canvas nav");
+    navPlace.innerHTML = mainNav.innerHTML;
+    // copy .header-top .wrapper to .thetop-nav
+    let topNav =document.querySelector(".header-top");
+    let topPlace =document.querySelector(".off-canvas .thetop-nav");
+    topPlace.innerHTML = topNav.innerHTML;
+
+}
+copyMenu();
