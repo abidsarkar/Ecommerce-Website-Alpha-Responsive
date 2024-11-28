58c160685461997111a4d336c30c802f89e8bdde
abidsarkar <abidsarkar0@gmail.com>
2024-11-28 12:00:03 +0600

    slider done

--- script.js
+++ script.js
@@ -53,8 +53,8 @@ const swiper = new Swiper('.swiper', {
   },
 
   // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
+//   scrollbar: {
+//     el: '.swiper-scrollbar',
+//   },
   });
   
